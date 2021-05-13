b1ccb5273ba3eb88eda030428ab3169669f483ef
heartrainy <526132448@qq.com>
2021-05-13 17:04:30 +0800

    提交

---
+++ .umirc.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: '心语的博客',
+  mode: 'doc',
+  description: '这是描述',
+  logo: '/images/logo.jpeg'
+  // more config: https://d.umijs.org/config
+});
---
+++ docs/help.zh-CN.md
@@ -0,0 +1,9 @@
+---
+title: 帮助
+nav:
+  title: 帮助
+---
+
+# 帮助
+
+Balabala...
