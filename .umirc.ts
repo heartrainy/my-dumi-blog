6000c475b44e67474e93dc20acf22c9a50594c0d
heartrainy <526132448@qq.com>
2021-08-30 14:44:11 +0800

    提交

--- .umirc.ts
+++ .umirc.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     // null, // null 值代表保留约定式生成的导航，只做增量配置
     {
       title: 'GitHub',
-      path: 'https://github.com/umijs/dumi',
+      path: 'https://github.com/heartrainy',
     },
     {
       title: '问题集',
